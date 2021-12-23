81ee0858c8384e3e0fc2febe05a3599876709dea
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2021-12-23 11:58:35 +0800

    add parallax slider

--- src/js/pages/swiferparallax.js
+++ src/js/pages/swiferparallax.js
@@ -1,40 +1,96 @@
-import "../../scss/pages/swifergsap3.scss";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
+import "../../scss/pages/swiferparallax.scss";
+import "swiper/css/bundle";
 import gsap from "gsap/all";
-import Swiper, { Navigation, Pagination, Autoplay } from "swiper";
-Swiper.use([Navigation, Pagination, Autoplay]);
+import Swiper from "swiper/bundle";
 
-const swiferBannerContainer = document.querySelector("#banner-slider");
-const swiperBanner = new Swiper(".banner-slider", {
-	direction: "horizontal",
-	// parallax: true,
-	slidesPerView: 1,
-	speed: 1500,
-	spaceBetween: 0,
+let mainSliderSelector = ".main-slider",
+	navSliderSelector = ".nav-slider",
+	interleaveOffset = 0.5;
+
+// Main Slider
+let mainSliderOptions = {
 	loop: true,
-	disableOnInteraction: true,
+	speed: 1000,
 	autoplay: {
-		delay: 5000,
+		delay: 3000,
 	},
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
+	loopAdditionalSlides: 10,
+	grabCursor: true,
+	watchSlidesProgress: true,
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
-	scrollbar: {
-		el: ".swiper-scrollbar",
-	},
-});
+	on: {
+		init: function () {
+			this.autoplay.stop();
+		},
+		imagesReady: function () {
+			this.el.classList.remove("loading");
+			this.autoplay.start();
+		},
+		slideChangeTransitionEnd: function () {
+			let swiper = this,
+				captions = swiper.el.querySelectorAll(".caption");
+			for (let i = 0; i < captions.length; ++i) {
+				captions[i].classList.remove("show");
+			}
+			swiper.slides[swiper.activeIndex]
+				.querySelector(".caption")
+				.classList.add("show");
+		},
+		progress: function () {
+			let swiper = this;
+			for (let i = 0; i < swiper.slides.length; i++) {
+				let slideProgress = swiper.slides[i].progress,
+					innerOffset = swiper.width * interleaveOffset,
+					innerTranslate = slideProgress * innerOffset;
 
-swiferBannerContainer.addEventListener("mouseenter", (e) => {
-	swiperBanner.autoplay.stop();
-});
-swiferBannerContainer.addEventListener("mouseleave", (e) => {
-	swiperBanner.autoplay.start();
-});
+				swiper.slides[i].querySelector(".slide-bgimg").style.transform =
+					"translateX(" + innerTranslate + "px)";
+			}
+		},
+		touchStart: function () {
+			let swiper = this;
+			for (let i = 0; i < swiper.slides.length; i++) {
+				swiper.slides[i].style.transition = "";
+			}
+		},
+		setTransition: function (speed) {
+			let swiper = this;
+			for (let i = 0; i < swiper.slides.length; i++) {
+				swiper.slides[i].style.transition = speed + "ms";
+				swiper.slides[i].querySelector(
+					".slide-bgimg"
+				).style.transition = speed + "ms";
+			}
+		},
+	},
+};
+let mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);
+
+// Navigation Slider
+let navSliderOptions = {
+	loop: true,
+	loopAdditionalSlides: 10,
+	speed: 1000,
+	spaceBetween: 5,
+	slidesPerView: 5,
+	centeredSlides: true,
+	touchRatio: 0.2,
+	slideToClickedSlide: true,
+	direction: "vertical",
+	on: {
+		imagesReady: function () {
+			this.el.classList.remove("loading");
+		},
+		click: function () {
+			mainSlider.autoplay.stop();
+		},
+	},
+};
+let navSlider = new Swiper(navSliderSelector, navSliderOptions);
+
+// Matching sliders
+mainSlider.controller.control = navSlider;
+navSlider.controller.control = mainSlider;
